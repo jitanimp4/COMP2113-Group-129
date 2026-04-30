0c612f30bcb6ea23e2012af52ac55222a0b16f67
menaka-menon <u3650575@connect.hku.hk>
2026-04-30 20:11:45 +0800

    Add files via upload

---
+++ word pyramid app/main.cpp
@@ -0,0 +1,18 @@
+#include "word_pyramid.h"
+
+using namespace std;
+
+/**
+ * Program entry point; starts the Word Pyramids game.
+ * Input parameters:
+ * - argc: count of command line arguments (unused).
+ * - argv: argument list (unused).
+ * Return value:
+ * - 0 on normal termination.
+ */
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+  runWordPyramidGame();
+  return 0;
+}
