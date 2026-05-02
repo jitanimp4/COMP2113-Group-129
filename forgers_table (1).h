ad42b52b5d49cc726d6de882b9a1b1d556091167
arpitaxys <arpitash41@gmail.com>
2026-05-02 14:46:23 +0800

    Delete forgers_table (1).h

--- forgers_table (1).h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// forgers_table.h
-// Exposes The Forger's Table minigame as a single callable function.
-// Returns true if the player completed a full round, false if they quit early (Q).
-bool runForgersTable();
